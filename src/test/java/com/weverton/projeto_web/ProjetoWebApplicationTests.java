28ca038e5f10aae800e9f3289e8932a0abf88e9e
weverton1988 <wtonja@gmail.com>
2019-12-12 09:45:57 -0300

    Project created

---
+++ src/test/java/com/weverton/projeto_web/ProjetoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weverton.projeto_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
